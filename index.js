1ce293e79636437ede27fe86ee6bf9f76f912f9d
gulu43 <gulamnabi9th@gmail.com>
2025-07-26 15:08:36 +0530

    backend basic working

--- index.js
+++ index.js
@@ -9,13 +9,32 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND
+    origin: process.env.FRONTNED
 }));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('nice');
 });
 
+app.post('/donate', (req, res) => {
+    const { name, amount } = req.body;
+    console.log("name from backend", name);
+    console.log("amount from backend", amount);
+
+    if (!name || !amount) {
+        return res.status(400).json({ error: "Name and amount are empty" });
+    }
+
+    res
+        .status(201)
+        .json({
+            'name': name,
+            'amount': amount
+        })
+
+})
 app.listen(PORT, () => {
     console.log("Server is listing", PORT);
 })
