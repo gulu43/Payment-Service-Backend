839855842415509969abd9d970b3568b919412a5
gulu43 <gulamnabi9th@gmail.com>
2025-08-06 12:19:48 +0530

    added create order code

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ app.get('/', (req, res) => {
 });
 
 app.post('/donate', (req, res) => {
-    const { name, amount } = req.body;
+    const { name, amount } = req.body 
     console.log("name from backend", name);
     console.log("amount from backend", amount);
 
     if (!name || !amount) {
         return res.status(400).json({ error: "Name and amount are empty" });
     }
-    if (amount < 0) {
-        return res.status(400).json({ error: "amount can not be zero" });
+    if (amount <= 0) {
+        return res.status(400).json({ error: "Amount must be greater than zero" });
     }
 
     res
@@ -41,31 +41,36 @@ app.post('/donate', (req, res) => {
 })
 
 // Initialize Razorpay instance
+// console.log(` key id, ${process.env.KEY_ID}`);
+// console.log(`secret id, ${process.env.KEY_SECRET}`);
+
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: `${process.env.KEY_ID}`,
+  key_secret: `${process.env.KEY_SECRET}`,
 });
 
 // Route to create Razorpay order
 app.post('/create-order', async (req, res) => {
   const { name, amount } = req.body;
-
+  
   const options = {
-    amount: amount * 100, // in paise
+    amount: Number(amount) * 100, // in paise
     currency: 'INR',
     receipt: `donation_${name}_${Date.now()}`,
   };
 
   try {
+    console.log('is it there' ,razorpay);
+    
     const order = await razorpay.orders.create(options);
     res.json({ orderId: order.id });
   } catch (err) {
     console.error(err);
-    res.status(500).send("Error creating order");
+    res.status(500).json({ error: "Error creating order"});
   }
 });
 
 app.listen(PORT, () => {
-    console.log("Server is listing", PORT);
+    console.log(`Server is listening on port ${PORT}`);
 })
 
